4f64c576faf7e43b27637e35e5f02444b72f1892
sparetype <griffin@sparetype.com>
2013-11-27 03:59:20 +0000

    Homepage recent blocks, grid customization

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 
 </head>
     
-<body <?php body_class('wrap wider'); ?>>
+<body <?php body_class(''); ?>>
     
 <?php wp_nav_menu( array('theme_location' => 'primary-navigation', 'container' => false, 'menu_id' => 'menu', 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>' )); ?>
 
