c21964ea82e56176625510528d723d5b3998a1cd
sparetype <griffin@sparetype.com>
2013-12-07 22:26:27 +0000

    Remove title on pages

--- page.php
+++ page.php
@@ -15,11 +15,6 @@ get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
 
-<header class="entry-header">
-
-<h1 class="entry-title"><?php the_title(); ?></h1>
-
-</header><!-- .entry-header -->
 
 <div class="entry-content grid">
 
