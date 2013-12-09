4350eb510730d16c1ef71f5c12423185c2391acc
sparetype <griffin@sparetype.com>
2013-12-09 22:37:14 +0000

    Single post grid and bottom margin

--- single.php
+++ single.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('unit whole no-gutters'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('grid'); ?>>
 
 <header class="entry-header">
 
