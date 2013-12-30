6471b24f81bd666ed0a24d499e7df867b9a42e08
sparetype <griffin@sparetype.com>
2013-12-30 22:50:48 +0000

    Single project pages added

---
+++ single-sparetype_project.php
@@ -0,0 +1,116 @@
+<?php
+
+/** The individual project page. */
+
+get_header(); ?>
+
+<div class="project-content" role="main">
+
+    
+<?php if ( have_posts() ) : ?>
+
+<?php /* Start the Loop */ ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('grid'); ?>>
+
+<div class="entry-content">
+
+<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>' ) ); ?>
+
+<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:' ) . '</span>', 'after' => '</div>' ) ); ?>
+
+</div><!-- .entry-content -->
+
+
+<footer class="entry-meta">
+    
+<p class="date-posted"><?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?></p>
+
+<?php $categories_list = get_the_category_list( __( ', ' ) );
+
+if ( $categories_list ): ?>
+
+<p class="cat-links">
+
+<?php printf( __( '<span class="%1$s">Category :</span> %2$s' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list ); ?>
+
+</p>
+
+<?php endif; // End if categories ?>
+
+
+<?php $tags_list = get_the_tag_list( '', __( ', ' ) );
+
+if ( $tags_list ): ?>
+
+<p class="tag-links">
+
+<?php printf( __( '<span class="%1$s">Tag :</span> %2$s' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+
+</p>
+
+<?php endif; // End if $tags_list ?>
+
+
+
+</footer><!-- #entry-meta -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
+
+
+<?php endwhile; ?>
+
+<?php endif; ?>
+
+
+<?php comments_template(); ?>
+
+
+</div><!-- #content -->
+
+
+<div id="pagination" role="navigation">
+
+<?php
+
+global $wp_query;
+
+$big = 999999999; // need an unlikely integer
+
+echo paginate_links( array(
+
+	'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+
+	'format' => '?paged=%#%',
+
+	'current' => max( 1, get_query_var('paged') ),
+
+	'total' => $wp_query->max_num_pages,
+
+	'prev_text'    => __('&#171;&#171; Newer Posts'),
+
+	'next_text'    => __('Older Posts &#187;&#187;'),
+
+) );
+
+?>
+
+</div> <!-- end #pagination -->
+
+
+
+<div id="sidebar-2">
+
+<?php if ( !function_exists('dynamic_sidebar')
+
+|| !dynamic_sidebar('sidebar-2') ) : ?>
+
+<?php endif; ?>
+
+</div> <!-- end #sidebar-2 -->
+
+
+
+<?php get_footer(); ?>
