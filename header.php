7e22d1703b0e101ad3158577d8a6d1200f0a1dd1
sparetype <griffin@sparetype.com>
2014-05-15 20:55:02 +0000

    Overhaul, restart, crash, burn, did not use git correctly.

--- header.php
+++ header.php
@@ -24,38 +24,34 @@
 
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
-<?php wp_enqueue_script( 'jpanelmenu', get_template_directory_uri().'/scripts/jquery.jpanelmenu.min.js', array('jquery'), '1.30', true ); ?>
-    
+<?php wp_enqueue_script( 'responsive-nav', get_template_directory_uri().'/scripts/responsive-nav.min.js', array(), '', false ); ?>
 
+<?php wp_enqueue_script( 'fastclick', get_template_directory_uri().'/scripts/fastclick.js', array(), '', true ); ?>
+	
+<?php wp_enqueue_script( 'scroll', get_template_directory_uri().'/scripts/scroll.js', array(), '', true ); ?>
+	
+<?php wp_enqueue_script( 'fixed-responsive-nav', get_template_directory_uri().'/scripts/fixed-responsive-nav.js', array(), '', true ); ?>
+	
 <?php wp_head(); ?>
-
 
 </head>
     
 <body <?php body_class(''); ?>>
 
-	<ul id="menu" class="menu">
+
+	<header id="site-navigation" class="site-navigation">
+	<a href="<?php echo home_url(); ?>" class="home-logo"><img src="<?php bloginfo('template_directory'); ?>/images/st_sparetype_logo_white.svg" /></a>
+		
+	<nav class="nav-collapse">
+
+	<ul>
 		
 <?php wp_nav_menu( array('theme_location' => 'primary-navigation', 'container' => false, 'menu_id' => '', 'items_wrap' => '%3$s' )); ?>
 	
-<li><form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-
-<input class="textbox" value="Search" name="s" id="s" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}" type="text">
-
-        <input type="submit" id="searchsubmit" value="&#128269;" />
-
-</form></li>
 	</ul>
-
-<ul id="navigation" role="navigation">
-
-<li><a href="#" class="menu-trigger">&#9776;</a></li>
-
-<li><a href="<?php echo home_url(); ?>" class="home-logo"><img src="<?php bloginfo('template_directory'); ?>/images/st_sparetype_logo.svg" /></a></li>
+</nav>
+	</header>
 	
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-1') ) : ?>
 
 <?php endif; ?>
-
-
-</ul> <!-- end #navigation -->
