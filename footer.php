d970ab8c43bfb8f48e590bf1d527b6bf6965be1d
sparetype <griffin@sparetype.com>
2014-07-23 01:29:22 +0000

    Footer spacing andclasses

--- footer.php
+++ footer.php
@@ -3,22 +3,22 @@
 ?>
 <div id="footer" role="footer">
 
-<div id="sidebar-1">
+<div id="sidebar-1" class="sidebar">
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-1') ) : ?>
 <?php endif; ?>
 </div> <!-- end #sidebar-1 -->
 	
-<div id="sidebar-2">
+<div id="sidebar-2" class="sidebar">
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-2') ) : ?>
 <?php endif; ?>
 </div> <!-- end #sidebar-2 -->
 	
-<div id="sidebar-3">
+<div id="sidebar-3" class="sidebar">
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-3') ) : ?>
 <?php endif; ?>
 </div> <!-- end #sidebar-3 -->
 
-<div id="sidebar-4">
+<div id="sidebar-4" class="sidebar">
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-4') ) : ?>
 <?php endif; ?>
 </div> <!-- end #sidebar-4 -->
