a1ed84fe512ac9c7e732c0e256c9df2a475677c7
sparetype <griffin@sparetype.com>
2014-08-04 17:43:56 +0000

    Favicons added

--- header.php
+++ header.php
@@ -14,6 +14,22 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/<?php echo get_stylesheet_directory_uri(); ?>/faviconapple-touch-icon-152x152.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-32x32.png" sizes="32x32">
+<meta name="msapplication-TileColor" content="#fec34d">
+<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/favicon/mstile-144x144.png">
+
 
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
