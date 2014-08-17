9025c97cfe71167edf464bcd29c447e96a0eefb1
sparetype <griffin@sparetype.com>
2014-08-17 17:53:13 +0000

    Grunt config for concat and uglify finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,24 @@
 module.exports = function(grunt) {
-	
-	// Project configuration.
-	grunt.initConfig({
-	uglify: {
-	build: {
-	src: 'scripts/responsive-nav.min.js',
-	dest: 'sparetype2014/scripts/responsive-nav.min.js'
-	}
-	}
-	});
-	
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	
-	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
-	
-	};
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['scripts/*.js' // All JS in the scripts folder
+                ],
+                dest: 'scripts/production.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: 'scripts/production.js',
+                dest: 'sparetype2014/scripts/production.min.js'
+            }
+        }
+    });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Default tasks.
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
