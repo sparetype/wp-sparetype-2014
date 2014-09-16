720d510063977235c34e2c7da75a7ec69ec8209f
sparetype <griffin@sparetype.com>
2014-09-16 00:49:50 +0000

    Project title

--- sparetype2014/single-sparetype_project.php
+++ sparetype2014/single-sparetype_project.php
@@ -15,6 +15,17 @@ get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+<header class="entry-header">
+
+<hgroup>
+
+<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+</hgroup>
+
+</header><!-- .entry-header -->
+
+
 <div class="entry-content">
 
 <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>' ) ); ?>
