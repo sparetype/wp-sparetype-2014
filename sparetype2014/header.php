5f484c5b9099ece290d824cc3c8e72f05dc41af8
Griffin Shreves, III <griffin@sparetype.com>
2021-04-16 18:18:13 +0000

    Nit picking changes that I should have been more disciplined about tracking

--- sparetype2014/header.php
+++ sparetype2014/header.php
@@ -21,7 +21,7 @@
 <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-120x120.png">
 <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/<?php echo get_stylesheet_directory_uri(); ?>/faviconapple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon-152x152.png">
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-196x196.png" sizes="196x196">
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-160x160.png" sizes="160x160">
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-96x96.png" sizes="96x96">
